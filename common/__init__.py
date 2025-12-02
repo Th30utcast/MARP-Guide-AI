4b0727902aad70973c568debeaf5a8790074f256
Valentin Toscano <valentin.toscano@hotmail.com>
2025-12-02 16:14:29 +0100

    CI issues

--- common/__init__.py
+++ common/__init__.py
@@ -3,8 +3,6 @@ Common utilities for MARP Guide AI services.
 """
 
 # This file makes 'common' a Python package
-# Import key classes for easier access
-
 from .mq import RabbitMQEventBroker
 
 __all__ = ["RabbitMQEventBroker"]
